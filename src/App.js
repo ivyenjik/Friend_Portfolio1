3efae8903a07dca10579cd01435e0a96415afd2e
Ivana Njike <ivananjike@gmail.com>
2024-03-14 11:53:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Commençons le portfolio de Yossa! Let's gooo....
         </p>
         <a
           className="App-link"
